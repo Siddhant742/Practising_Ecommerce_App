6883d3dff96792e01d0baf50e2e9c2331d118c05
Siddhant <076bct083@ioepc.edu.np>
2023-07-15 16:07:56 +0545

    Initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+Color kDarkGreenColor = const Color(0xFF184A2C);
+Color kGinColor = const Color(0xFFE5F0EA);
+Color kSpiritedGreen = const Color(0xFFC1DFCB);
+Color kFoamColor = const Color(0xFFEBFDF2);
+Color kGreyColor = Colors.grey.shade600;
+Color kMain =  const Color(0xff00ad59);
+Color kWhite =  const Color(0xffffffff);
+Color kSecondary =  const Color(0xff626463);
