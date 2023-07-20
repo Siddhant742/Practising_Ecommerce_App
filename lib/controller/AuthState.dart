5644ef1e19c22c754a3b94f16e873a4b69345cb4
Siddhant <076bct083@ioepc.edu.np>
2023-07-20 20:27:25 +0545

    bloc

--- lib/controller/AuthState.dart
+++ lib/controller/AuthState.dart
@@ -3,3 +3,4 @@ abstract class AuthState{}
 class AuthInitialState extends AuthState{}
 class AuthLoadingState extends AuthState{}
 class AuthLggedInState extends AuthState{}
+class AuthLoggedOutState extends AuthState{}
